ea2c6195f259b18916a349fd0a3e6e29a2cfe6ac
Lucas Blomhäll <lucas.blomhall@gmail.com>
2022-09-10 13:06:32 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppOOPYouTubetutorial
+{
+    class Person
+    {
+        //field
+        private string Personality;
+
+        //Constructor
+        public Person()
+        {
+            Personality = "Mystery";
+        }
+
+        public void AskQuestion(int answer)
+        {
+            if (answer == 1)
+            {
+                Personality = "Extrovert";
+            }
+            else if(answer == 2)
+            {
+                Personality = "Introvert";
+            }
+            else
+            {
+                Personality = "You are still a mystery";
+            }
+        }
+        //This method returns the value of personality
+        public string GetPersonality()
+        {
+            return Personality;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleAppOOPYouTubetutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input;
+
+                Console.WriteLine("Type 1 talk to others. Type 2 if you talk to yourself :)");
+                input = Console.ReadLine();
+                Person myperson = new Person();
+                myperson.AskQuestion(int.Parse(input));
+                Console.WriteLine("You are: " + myperson.GetPersonality());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an interger value.");
+                
+            }
+            
+        }
+    }
+}
